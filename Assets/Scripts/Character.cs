9aae46777e8c322d62398e6a9e7520644841593b
pojanicha <pojanicha.sora@bumail.net>
2025-10-14 11:00:45 +0700

    Add All Character Script

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    //attribute
+
+    private int health;
+    public int Health { get => health; set => health = (value < 0) ? 0 : value; }
+
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+    //Methods
+
+    public void TakeDamage(int damage)
+    { 
+        Health -= damage;
+        Debug.Log($"{this.name} took damage {damage}.Current Health : {Health}");
+    
+    }
+
+
+    public bool IsDead()
+    {
+        if (Health <= 0)
+        {
+            Destroy(this.gameObject);
+            Debug.Log($"{this.name} is dead TT");
+            return true;
+        
+        
+        }
+        else return false;
+    
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
