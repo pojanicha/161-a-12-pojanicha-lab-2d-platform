195b73b87eb8809cb207871cf6e7e053c9d8b1d4
pojanicha <pojanicha.sora@bumail.net>
2025-10-28 09:59:59 +0700

    Rock and Banana

---
+++ Assets/Scripts/Banana.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Banana : Weapon
+{
+    public override void Move()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnHitWith(Character character)
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,12 +4,45 @@ public abstract class Weapon : MonoBehaviour
 {
 
     public int damage;
-    //public IShootable Shooter;
+    public IShootable Shooter;
 
 
     public abstract void Move();
 
     public abstract void OnHitWith(Character character);
+
+
+
+    public void InitWeapon(int newDamage, IShootable newShooter)
+    { 
+        damage = newDamage;
+        Shooter = newShooter;
+    
+    }
+
+    public int GetShootPoint()
+    { 
+
+        float value = Shooter.shootPoint.position.x - Shooter.shootPoint.parent.position.y;
+
+        if (value < 0)
+            return 1;
+        else return -1;
+    
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Character character = other.GetComponent<Character>();
+
+        if (character != null)
+        {
+            OnHitWith(character);
+            Destroy(this.gameObject, 5f);
+        
+        }
+    }
+
 
 
 
